257bdbd0940a144ddc8e429db9a5306b852522ed
abel.lopez <abel.lopez@benchmarkemail.com>
2024-02-06 19:12:30 -0400

    add env

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  imageProviders: {
+    unsplash: {
+      apiUrl: 'https://api.unsplash.com',
+      apiKey: 'UNSPLASH_API_KEY'
+    },
+    pexels: {
+      apiUrl: 'https://api.pexels.com/v1',
+      apiKey: 'PEXELS_API_KEY'
+    },
+    pixabay: {
+      apiUrl: 'https://pixabay.com/api',
+      apiKey: 'PIXABAY_API_KEY'
+    },
+  }
+};
